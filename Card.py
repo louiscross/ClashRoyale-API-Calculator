b1583f06773eef2f46875b076c2d4d2adb6495c1
Louiscross13 <118275848+Louiscross13@users.noreply.github.com>
2023-03-20 20:17:32 +0000

    Update Card.py

--- Card.py
+++ Card.py
@@ -12,7 +12,7 @@ class Card:
 
 headers = {
     "Accept": "application/json",
-    "authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFmNDk2M2NlLTU3NjctNGYyYi1hNDYzLWEwOGM5OGYxNjZkYSIsImlhdCI6MTY3OTIyNDkyOSwic3ViIjoiZGV2ZWxvcGVyL2YxYzQ1ZTg1LTc4NjQtOGJjYi00OWE4LTNjZGU4YzJhYjYxNiIsInNjb3BlcyI6WyJyb3lhbGUiXSwibGltaXRzIjpbeyJ0aWVyIjoiZGV2ZWxvcGVyL3NpbHZlciIsInR5cGUiOiJ0aHJvdHRsaW5nIn0seyJjaWRycyI6WyIxODguMjEzLjEzOC4yMTYiXSwidHlwZSI6ImNsaWVudCJ9XX0.70WZgFaB9Rix56YxoKzh-BHJOBbu_V3s3imUxxD6wSay5kWZMkmNL3Xn82HVPHXxTa_oXNbLtdZMFY1CV-Z02g"
+    "authorization": "Bearer "+auth
 
 }
 
